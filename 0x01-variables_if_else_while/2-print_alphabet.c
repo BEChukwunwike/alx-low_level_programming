fb2b50b3efa35627948c3e305ba9b3a7041083fd
BEChukwunwike <blessingechukwunwike@gmail.com>
2022-12-09 16:24:37 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
