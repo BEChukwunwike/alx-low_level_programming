c03e8fc4084d518272f98dab37addfc72612b44c
BEChukwunwike <blessingechukwunwike@gmail.com>
2022-12-13 14:45:29 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (letr = 'a'; letr <= 'z'; letr++)
-			_putcher(letr);
-		_putcher('\n');
+			_putchar(letr);
+		_putchar('\n');
 	}
 }
