951d3c2a22bb5cda38ce1d08b3cce19ee89ec527
BEChukwunwike <blessingechukwunwike@gmail.com>
2022-12-15 09:59:42 -0800

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,17 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest
+	int largest;
 
 	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
