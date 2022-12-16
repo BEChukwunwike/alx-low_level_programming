becb97863511f2e2d2c2b172895be97c6f4a5b07
BEChukwunwike <blessingechukwunwike@gmail.com>
2022-12-15 16:49:26 -0800

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * @num- numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int num = '0';
+
+	while (num <= 9)
+	{
+		_putchar(num);
+			num++;
+	}
+		_putchar('\n');
+}
