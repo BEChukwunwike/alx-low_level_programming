b738cf721b9070c27353270124552e056a070c35
BEChukwunwike <blessingechukwunwike@gmail.com>
2022-12-22 11:16:06 -0800

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c;
+	int p;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	p = 0;
+	while (p < n && src[p] != '\0')
+	{
+		dest[c] = src[p];
+		c++;
+		p++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
