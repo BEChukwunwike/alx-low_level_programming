39b8172ce3e6b33c33ac1e78589b15644dd03f23
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-01-06 16:39:26 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: input value
+ * @src: input value
+ * Return: returns a pointer to the
+ * resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c;
+	int n;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[c] = src[n];
+		c++;
+		n++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
