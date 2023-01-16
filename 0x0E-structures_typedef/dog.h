b3a3ff7c1da960c7265ebefd6ab1b35a441510b5
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-01-16 07:33:12 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's details
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * @dog_t - new type
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
