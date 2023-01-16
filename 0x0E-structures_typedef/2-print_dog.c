b7cce3f91cba45d692631fb6a5fbd289f72a2c99
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-01-16 12:10:34 -0800

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,14 +9,21 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == 0)
+	{
 		printf("Name: (nil)\n");
+	}
 	else
+	{
 		printf("Name: %s\n", d->name);
-
+	}
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == 0)
+	{
 		printf("Owner: (nil)\n");
+	}
 	else
+	{
 		printf("Owner: %s\n", d->owner);
+	}
 }
