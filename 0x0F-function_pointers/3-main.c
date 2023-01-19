7663fec71a40c32f2b00e7cab2faccd0701f5ad0
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-01-19 03:28:54 -0800

    :task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,8 +10,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -20,8 +18,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -31,14 +27,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	o = *argv[2];
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	result = func(arg1, arg2);
-	printf("%d\n", result);
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
 
