132fc02a0958c1938a64c2156cfb5eb1108d357f
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-03-20 10:45:34 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - computes the absolute value of an integer
+ *@a: the intiger to be computed
+ * Return: the absolute value
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
